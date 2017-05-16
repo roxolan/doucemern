6b45cedcc6b9c0ffe2e3174367a16bb0461447c9
Viktor Kotusenko <roxolanus@gmail.com>
2017-05-16 01:17:36 -0400

    using babel-cli to transpile jsx and serving static assets with express

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
-app.use(express.static('static'))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static', 'index.html'))
+})
+
+app.use(express.static(path.join(__dirname, 'static')))
+app.use(express.static(path.join(__dirname, 'node_modules/react/dist')))
+app.use(express.static(path.join(__dirname, 'node_modules/react-dom/dist')))
 
 app.listen(8888, function() {
   console.log('App started on port 8888')
---
+++ static/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var div = React.DOM.div;
+var h1 = React.DOM.h1;
+
+var MyTitle = React.createClass({
+  render: function render() {
+    return div(null, h1(null, 'Yob Search Platform'));
+  }
+});
+
+var MyFirstComponent = div(null, React.createElement(MyTitle, null), React.createElement(MyTitle, null), React.createElement(MyTitle, null));
+
+ReactDOM.render(MyFirstComponent, document.getElementById('app'));
