fb8c0c819565ce8f29df41d99116626e9a7f6663
Viktor Kotusenko <roxolanus@gmail.com>
2017-05-24 23:19:41 -0400

    remove unnecessary express.static folders after vendor.bundle is now build

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ app.get('/', (req, res) => {
 })
 
 app.use(express.static(path.join(__dirname, 'static')))
-app.use(express.static(path.join(__dirname, 'node_modules/babel-polyfill')))
-app.use(express.static(path.join(__dirname, 'node_modules/react/dist')))
-app.use(express.static(path.join(__dirname, 'node_modules/react-dom/dist')))
 
 app.listen(8888, function () {
   console.log('App started on port 8888')
