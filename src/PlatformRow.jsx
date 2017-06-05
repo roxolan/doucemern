95b9988b12d79dabe8e54537f502313b40ca11fe
Viktor Kotusenko <roxolanus@gmail.com>
2017-06-04 23:36:03 -0400

    fixing styling

--- src/PlatformRow.jsx
+++ src/PlatformRow.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 class PlatformRow extends React.Component {
   render () {
-    const borderedStyle = {border: "px solid silver", padding: 4}
+    const borderedStyle = {border: "1px solid silver", padding: 4}
     return (
       <tr>
         <td style={borderedStyle}>{this.props.platform_id}</td>
