ec491d6f843705f7ca943918b5b03f6a067758e7
Viktor Kotusenko <roxolanus@gmail.com>
2017-06-06 23:55:33 -0400

    adding state

--- src/app.jsx
+++ src/app.jsx
@@ -25,13 +25,34 @@ const platforms = [
 ]
 
 class PlatformList extends Component {
+  constructor () {
+    super()
+    this.state = { platforms: platforms }
+
+    setTimeout(this.createTestPlatform.bind(this), 2000)
+  }
+
+  createPlatform (newPlatform) {
+    const newPlatforms = this.state.platforms.slice()
+    newPlatform.id = this.state.platforms.length + 1
+    newPlatforms.push(newPlatform)
+    this.setState({ platforms: newPlatforms })
+  }
+
+  createTestPlatform () {
+    this.createPlatform({
+      created: new Date(),
+      title: 'AngelList'
+    })
+  }
+
   render () {
     return (
       <div>
         <h1>Platform Tracker</h1>
         <PlatformFilter />
         <hr />
-        <PlatformTable platforms={platforms} />
+        <PlatformTable platforms={this.state.platforms} />
         <hr />
         <PlatformAdd />
       </div>
